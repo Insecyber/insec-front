1b9a9c9dbe23f5a53ff95d408d70b69648e1b857
Tukutlas <victoradameji2014@gmail.com>
2022-09-27 21:45:12 +0100

    mobile maintenance pop up page

--- js/device.js
+++ js/device.js
@@ -1,17 +1,10 @@
 const deviceType = () => {
     const ua = navigator.userAgent;
     if (/(tablet|ipad|playbook|silk)|(android(?!.*mobi))/i.test(ua)) {
-        // return "tablet";
-        console.log("tablet");
-        // window.location.href('https://www.insecyber.com/mobile');
+        $('#maintenance').modal('show');
     }
     else if (/Mobile|Android|iP(hone|od)|IEMobile|BlackBerry|Kindle|Silk-Accelerated|(hpw|web)OS|Opera M(obi|ini)/.test(ua)) {
-        // return "mobile";
-        console.log("mobile");
-    }
-    // return "desktop";
-    else{
-        console.log("desktop");
+        $('#maintenance').modal('show');
     }
     
 };
